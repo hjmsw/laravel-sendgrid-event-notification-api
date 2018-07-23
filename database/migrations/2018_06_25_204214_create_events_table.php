475c5a3b898ce4a105e81129968c0db352123c72
James Harding <james@jamesharding.me>
2018-07-23 23:32:07 +0100

    Remove debug code. Add package discovery config. Add empty readme file.

--- database/migrations/2018_06_25_204214_create_events_table.php
+++ database/migrations/2018_06_25_204214_create_events_table.php
@@ -13,16 +13,6 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-//        {
-//            "email": "example@test.com",
-//        "timestamp": 1529959204,
-//        "smtp-id": "<14c5d75ce93.dfd.64b469@ismtpd-555>",
-//        "event": "processed",
-//        "category": "cat facts",
-//        "sg_event_id": "M8ciYYFr8lWt6iH4maWgWg==",
-//        "sg_message_id": "14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"
-//    },
-
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->string('email');
